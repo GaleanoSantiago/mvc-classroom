204982db081d26eee30feef86159b9e31a690736
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2024-11-03 02:31:54 -0300

    titulo de las paginas

--- view/template/header.php
+++ view/template/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title><?php echo $controller->titulo_pagina; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous">
